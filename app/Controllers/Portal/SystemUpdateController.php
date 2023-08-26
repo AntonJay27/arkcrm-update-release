e671471ed4515d00f4bfc71d94f224206061eddf
AntonJay27 <ajhay.dev@gmail.com>
2023-08-26 11:02:10 +0800

    [2023-08-26] Update version

--- app/Controllers/Portal/SystemUpdateController.php
+++ app/Controllers/Portal/SystemUpdateController.php
@@ -11,7 +11,7 @@ class SystemUpdateController extends BaseController
       $output_array = null;
       $cmd_status = null;
 
-      exec('git pull github arkcrm-patch-v1-0-0:arkcrm-patch-v1-0-0', $output_array, $cmd_status);
+      exec('git pull github arkcrm-patch-v1-0-1:arkcrm-patch-v1-0-1', $output_array, $cmd_status);
 
       if(count($output_array) > 0)
       {
@@ -30,7 +30,7 @@ class SystemUpdateController extends BaseController
       $output_array = null;
       $cmd_status = null;
 
-      exec('git checkout arkcrm-patch-v1-0-0', $output_array, $cmd_status);
+      exec('git checkout arkcrm-patch-v1-0-1', $output_array, $cmd_status);
 
       $arrResult = [$output_array, $cmd_status];
 
